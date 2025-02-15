9eda4e5a1df20734c998cbda29c7d7422656a472
YBuddha Dev Sarker <bdev.sarker@gmail.com>
2025-02-15 17:44:38 -0500

    16 Getting Started with the Results Screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:basic2quiz/start_screen.dart';
 import 'package:basic2quiz/questions_screen.dart';
 import 'package:basic2quiz/data/questions.dart';
+import 'package:basic2quiz/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -29,7 +30,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       });
     }
   }
@@ -42,6 +43,10 @@ class _QuizState extends State<Quiz> {
       screenWidget = QuestionsScreen(
         onSelectAnswer: chooseAnswer,
       );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = const ResultsScreen();
     }
 
     return MaterialApp(
